6c42ad8acb3801ccc1cc9f3dcf0e0941fd0ecd72
yoryokk <34732193+yoryokk@users.noreply.github.com>
2017-12-21 14:55:49 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,8 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = '2Gx/H04PHFG4WFjfflBPbAPa/FoYR9Uf8uvHPS6xdEVHmw3IrK913vGDjMQuBC67tp1jXP2ZB0WnZ/ifsMlkv97BDtuRKP3p7DFKTwJIEGYPTUs9UiDzvQDLhQ8PQr2v6KkZQ+20mM8DKMeRwRKi0AdB04t89/1O/w1cDnyilFU=
+'; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +15,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'de736c21a7512b51531a76bbfcbeeb05',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
